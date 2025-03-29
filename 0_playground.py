7d6057978024c78ab99de840ea8cf15a9ba5bfc8
franců <francupatrik@seznam.cz>
2025-03-29 13:08:58 +0100

    33_keyword_arguments.py 4:15:37
    Formátování názvů souborů
    
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s

--- 0_playground.py
+++
@@ -1,5 +0,0 @@
-def get_phone(country_code, area_code, firstd, lastd):
-  return f"{country_code}-{area_code}-{firstd}-{lastd}"
-
-phone_num = get_phone(country_code=1, area_code=123, firstd=456, lastd=7890)
-print(phone_num)
