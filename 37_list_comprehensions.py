6f84450ff130401788918440dfc4b0538b444f07
franců <francupatrik@seznam.cz>
2025-03-30 16:01:35 +0200

    37_list_comprehensions.py 4:45:57
    membership_operators
    
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s

---
+++ 37_list_comprehensions.py
@@ -0,0 +1,3 @@
+# List comprehension = a concise way to create lists in Python
+#                      Compact and easier to read than traditional loops
+#                      [expression for value in iterable if condition]
