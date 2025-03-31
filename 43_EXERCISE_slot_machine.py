d0a6fa99fe6bde4d5678305d3b4a76cfe8897b91
franců <francupatrik@seznam.cz>
2025-03-31 14:54:16 +0200

    43_EXERCISE_slot_machine.py 5:43:52
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 43_EXERCISE_slot_machine.py
+++ 43_EXERCISE_slot_machine.py
@@ -1,1 +1,42 @@
-pass
+import random
+#SLOT MACHINE EXERCISE 🍒🍉🍋🔔⭐
+symbols = ("🍒", "🍉", "🍋", "🔔", "⭐")
+
+
+
+def spin_row():
+    pass
+
+
+
+def print_row():
+    pass
+
+
+
+def get_payout():
+    pass
+
+
+def main():
+    balance = 100
+
+    print("************************")
+    print("Welcome to Slot Machine!")
+    print("Symbols:🍒 🍉 🍋 🔔 ⭐")
+    print("************************")
+
+    while balance > 0: #While je balance víc jak 0, můžem pokračovat ve hře
+        print(f"Current balance: {balance} KČ")
+
+        bet = input("How much would you like to bet?: ")
+
+        if not bet.isdigit():
+            print("Please enter a valid number.")
+            continue
+
+
+
+if __name__ == "__main__":
+    main()
+
