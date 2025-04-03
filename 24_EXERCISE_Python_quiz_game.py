68fff9d2fe93ee336af9d86e276493646b4af3a8
franců <francupatrik@seznam.cz>
2025-04-03 09:55:40 +0200

    43_EXERCISE_slot_machine.py 5:44:27
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 24_EXERCISE_Python_quiz_game.py
+++ 24_EXERCISE_Python_quiz_game.py
@@ -57,3 +57,57 @@ print()
 
 score = int(score / len(questions) *100)
 print(f"Your score is {score}%")
+
+
+
+
+#Druhý pokus
+#Python quiz game
+
+'''
+otazky = (
+    "What is the capital of Germany?",  # Otázka 1
+    "How many fingers on humans hand?",  # Otázka 2
+    "Kdo honí?",  # Otázka 3
+    "Who wrote 'Romeo and Juliet'?",  # Otázka 4
+    "Co se učim?"  # Otázka 5
+)
+moznosti = ( #Možnosti jsou 2D Tuple
+    ("A. Berlin", "B. Paris", "C. Madrid", "D. Rome"),  # Možnosti pro otázku 1
+    ("A. 16", "B. 5", "C. 4", "D. 16"),  # Možnosti pro otázku 2
+    ("A. Zeme", "B. Mars", "C. Jupiter", "D. Saša"),  # Možnosti pro otázku 3
+    ("A. Mark Twain", "B. William Shakespeare", "C. Charles Dickens", "D. J.K. Rowling"),  # Možnosti pro otázku 4
+    ("A. Indian Ocean", "B. Atlantic Ocean", "C. Python", "D. Pacific Ocean")  # Možnosti pro otázku 5
+)
+odpovedi = ("A", "B", "D", "B", "C")
+pokusy = []
+score = 0
+question_num = 0 #Tato proměnná zjištuje v jaké otázce zrovna jsme, začínáme na indexu 0
+
+for otazka in otazky:
+    print("--------------------------------")
+    print(otazka)
+
+    for moznost in moznosti[question_num]: #moznosti [question_num] vybírá konkrétní tuple z moznosti na základě aktuální hodnoty question_num.
+#např Pokud question_num je 0, možnosti [question_num] je ("A. BERLIN, B. PARIS, C. MADRID. D ROME)
+
+        print(moznost)
+
+    guess = input("Choose your option: A, B, C, D ").upper()
+    pokusy.append(guess)
+
+    if guess == odpovedi[question_num]:
+        score += 1
+        print("Correct!")
+    else:
+
+        print("Inccorect!")
+        print(f"Correct answer is {odpovedi[question_num]}.") #[question_num] je index, dává to index a zvyšeuje se po každem ckylu
+    question_num += 1
+
+print("RESULTS")
+
+print(f"Guesses: {pokusy}")
+print(f"Correct: {odpovedi}")
+print(f"Body: {score}")
+'''
