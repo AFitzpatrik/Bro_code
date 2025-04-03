5e1ebbdad7d4d2b86b99d71201d2f716d07c3cc6
franců <francupatrik@seznam.cz>
2025-04-03 11:15:43 +0200

    43_EXERCISE_slot_machine.py 5:45:20
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 43_EXERCISE_slot_machine.py
+++ 43_EXERCISE_slot_machine.py
@@ -28,16 +28,22 @@ def main():
 
     while balance > 0: #While je balance víc jak 0, můžem pokračovat ve hře
         print(f"Current balance: {balance} KČ")
-
         bet = input("How much would you like to bet?: ")
-
         if not bet.isdigit():
             print("Please enter a valid number.")
             continue
 
         bet = int(bet)
 
+        if bet > balance:
+            print("Not enough funds.")
+            bet = input("How much would you like to bet?: ")
+            continue
+        if bet <= 0:
+            print("Bet must be greater than 0.")
+            continue
 
+    balance -= bet
 
 if __name__ == "__main__":
     main()
