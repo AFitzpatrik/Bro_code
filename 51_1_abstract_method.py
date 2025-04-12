774cecf50a331757c179a93daf92be060c2e53fd
franců <francupatrik@seznam.cz>
2025-04-13 00:20:49 +0200

    ABSTRACT METHOD - SOLO VIDEO :
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 51_1_abstract_method.py
@@ -0,0 +1,7 @@
+#Abstract class: A class that cannot be instantiated on tis own, Meant to be sublcassed.
+#                They can contain abstract methods, which are declared but have no implementation.
+#                1. Prevents instantiation of the class itself
+#                2. Requires children to use inherited methods
+
+
+
