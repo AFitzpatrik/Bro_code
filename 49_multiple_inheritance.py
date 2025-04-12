cc8ff1350967a67186bfc373adce90fdcf836ce0
franců <francupatrik@seznam.cz>
2025-04-12 11:06:20 +0200

    49_multiple_inheritance.py 7:00:03
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 49_multiple_inheritance.py
+++ 49_multiple_inheritance.py
@@ -2,3 +2,35 @@
 #               C(A, B)
 #multilevel inheritance = inherit from a parent which inherits from another parent
 #               C(B) <- B(A) <- A
+
+
+
+class Prey:
+    def flee(self):
+        print("This animal is fleeing!")
+
+
+
+class Predator:
+    def hunt(self):
+        print("This animal is hunting!")
+
+
+class Rabbit(Prey):
+    pass
+
+
+class Hawk(Predator):
+    pass
+
+
+class Fish(Prey, Predator):
+    pass
+
+
+rabbit = Rabbit()
+hawk = Hawk()
+fish = Fish()
+
+fish.flee()
+fish.hunt()
