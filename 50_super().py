610bf1155475703aa6a4a209c41c4b789b3e5fb7
franců <francupatrik@seznam.cz>
2025-04-12 11:47:28 +0200

    50_super().py 7:08:06
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 50_super().py
@@ -0,0 +1,62 @@
+# super() = Function usd in a child class to call methods from parent class (superclass)
+#   Allows you to extend the functionality of the inherited method
+
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+    def describe(self):
+        print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled) #tímto zavoláme init constructor z parent classy
+        self.radius = radius
+
+    def describe(self):
+        print(f"It is a circle withe and area of {3.14 * self.radius * self.radius} cm^2")
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+
+
+
+
+circle = Circle("red", True, 5)
+square = Square("blue", False, 6)
+triangle = Triangle("green", True, 7, 8)
+print(circle.color)
+print(circle.is_filled)
+print(f"{circle.radius} cm")
+print("")
+print(square.color)
+print(square.is_filled)
+print(f"{square.width} cm")
+print("")
+triangle.color = "yellow"
+print(triangle.color)
+print(triangle.is_filled)
+print(f"{triangle.width} cm")
+print(f"{triangle.height} cm")
+
+
+circle.describe()
+square.describe()
+triangle.describe()
+circle.describe()
+
+#V tomto příkladu má mají classy spoločné color,is filled ale pak mají jiné proměnné
+#Abychom nemuseli psát color a is_filled do každé classy, použijeme super()
+
+#pokud jsou dvě metohdy (v tomto případe describe) stejné, tak se zavolá ta z child classy, ne z parent classy
