c848f1c7bc87baddd652667f28002825288add97
franců <francupatrik@seznam.cz>
2025-04-12 10:38:03 +0200

    49_multiple_inheritance.py 7:00:03
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 48_inheritance.py
+++ 48_inheritance.py
@@ -15,15 +15,42 @@ class Son(Father): #zde dědí z class father proměnné height a color
 '''
 
 
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
+
+class Mouse(Animal):
+    def speak(self):
+        print("SQUEK!")
+
+
+dog = Dog("Scooby")
+cat = Cat("Garfield")
+mouse = Mouse("Mickey")
+
+dog.speak()
+cat.speak()
+mouse.speak()
 
 
 
-
-
-
-
-
-
+#Summary
+#Classy mohou od sebe dědit, hodí se to abychom nemuseli psát víc kódu, když se metody, atributy opakují.
 
 
 
