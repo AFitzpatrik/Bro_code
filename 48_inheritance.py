764baf89c071b967d8bda54e85bd27ba435e9c77
franců <francupatrik@seznam.cz>
2025-04-12 10:20:23 +0200

    48_inheritance.py 6:53:06
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 48_inheritance.py
@@ -0,0 +1,42 @@
+#Inheritance = Allow a  class to inherit attributes and methods from another class
+#              Help with code reusability and extensibility
+#              class Child(Parent):
+
+#poznámka k attributům a metodám(attributes and methods):
+#když mám def_něco mimo class, jedná se o funkci. Pokud mám def_něco unvnitř classy, je to metoda.
+
+'''
+class Father:
+    height = 182
+    color = "pink"
+
+class Son(Father): #zde dědí z class father proměnné height a color
+    pass
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#čas - 6:53:06
