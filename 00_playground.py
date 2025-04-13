b474f9fc16e32c147666b0404fe5cb5a1f367911
franců <francupatrik@seznam.cz>
2025-04-13 11:53:04 +0200

    ABSTRACT METHOD - SOLO VIDEO :
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 00_playground.py
+++ 00_playground.py
@@ -1,23 +1,24 @@
 class Clovek:
-    def __init__(self, jmeno, vek):
+    def __init__(self, jmeno, vek, vaha):
         self.jmeno = jmeno
         self.vek = vek
+        self.vaha = vaha
 
     def pohybuje_se(self):
         pass
 
 class Student(Clovek):
-    def __init__(self, jmeno, vek, prospech):
-        super().__init__(jmeno, vek)
+    def __init__(self,jmeno, vek, vaha, prospech):
+        super().__init__(jmeno, vek, vaha)
         self.prospech = prospech
 
     def uceni_se(self):
-        print(f"Student : {self.jmeno} se učí.Je mu {self.vek} a má prospěch {self.prospech}.")
+        print(f"Student : {self.jmeno} se učí.Je mu {self.vek}, váží {self.vaha} a má prospěch {self.prospech}.")
 
 
-student = Student("Jakub", 18, "vyznamenání")
-student2 = Student("Tonda", 18, "propadl")
 
-print(student.jmeno)
+
+student = Student("Petr", 20, 80, "výborný")
+student2 = Student("Eva", 22, 60, "dobrý")
 student.uceni_se()
 student2.uceni_se()
