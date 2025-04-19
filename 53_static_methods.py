8610f9b44013e67a2b0f424df73ba7de0e3d76e0
franců <francupatrik@seznam.cz>
2025-04-19 13:34:49 +0200

    53_static_methods.py 7:33:35
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 53_static_methods.py
@@ -0,0 +1,4 @@
+#Static methods = A method that belongs to a class rather than any object from that class(instance)
+# #                Usually used for general utility functions
+# Instance methods = Best for operations on instances of the class (objects)
+# Static methods = Best for utility fucntions that do not need access to class data
