dbf35efc3d1fe452b7951120a7ab75dfad8cabe5
franců <francupatrik@seznam.cz>
2025-04-19 12:29:54 +0200

    52_duck_typing.py 7:29:16
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 51_1_1_abstract_method_brocode.py
@@ -0,0 +1,6 @@
+#https://www.youtube.com/watch?v=ix9cRaBkVe0&t=26470s
+#https://www.youtube.com/watch?v=-buaqIbHHxo
+
+
+
+from abc import ABC, abstractmethod
---
+++ 52_duck_typing.py
@@ -0,0 +1,3 @@
+#Duck typing = Another way to achieve polymorphism besides Inheritance
+#               Object must have a minimum necessary attributes/methods
+#             = If it looks like a duck and quacks like a duck, it is a duck.
