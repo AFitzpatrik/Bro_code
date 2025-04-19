a7f5a4bd6f2aff36ef5ee5a6ba2bbb260faefb80
franců <francupatrik@seznam.cz>
2025-04-19 13:29:53 +0200

    52_duck_typing.py 7:29:16
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 52_duck_typing.py
+++ 52_duck_typing.py
@@ -1,3 +1,30 @@
 #Duck typing = Another way to achieve polymorphism besides Inheritance
 #               Object must have a minimum necessary attributes/methods
 #             = If it looks like a duck and quacks like a duck, it is a duck.
+
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
+
+class Car:
+    alive = False
+#    def horn(self): # Takto nebude fungovat
+    def speak(self):
+        print("HONK!")
+
+
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak() #Have every animal do a speak method
+    print(animal.alive) #Have every animal do a alive method
+
+
