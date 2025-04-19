5d620fe4f0b95d52efb633de4eb19a9aff4a2de4
franců <francupatrik@seznam.cz>
2025-04-19 13:47:15 +0200

    53_static_methods.py 7:33:35
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 53_static_methods.py
+++ 53_static_methods.py
@@ -2,3 +2,26 @@
 # #                Usually used for general utility functions
 # Instance methods = Best for operations on instances of the class (objects)
 # Static methods = Best for utility fucntions that do not need access to class data
+
+
+class Employee:
+    def __init__(self, name, postition):
+        self.name = name
+        self.position = position
+
+    def get_info(self):
+        return (f"{self.name} is a {self.position}")  # Instance method
+
+    @staticmethod
+    '''
+    Static method = belongs to a Class Employee, not to a object, created from that class
+    '''
+    def is_valid_position(position):
+        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+        return position in valid_positions #zde vracíme zda POSITION je IN VALID_POSITIONS listu, TRUE nebo FALSE
+
+
+print(Employee.is_valid_position("Cook"))
+
+print(Employee.is_valid_position("Nigga"))
+
