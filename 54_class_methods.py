5dda66f7080fcaf3c0a23a5ae922ea9013938648
franců <francupatrik@seznam.cz>
2025-04-21 16:24:11 +0200

    54_class_methods.py 7:39:32
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 54_class_methods.py
+++ 54_class_methods.py
@@ -1,2 +1,15 @@
 #Class methods = Allow operations related to the class itself
-# #             Take (cls) as the first parameter, which represents the class itself.
+# #             Take (cls) as the first parameter, which represents the class itself.fightnight challange
+# cls = class
+
+
+
+class Student:
+
+    count = 0  # Class variable to keep track of the number of students
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Student.count += 1 # Increment the count of students whenever a new student is created
+
