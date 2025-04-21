16d7f2303bdebf11e212865695b5ba583f196022
franců <francupatrik@seznam.cz>
2025-04-21 16:29:46 +0200

    54_class_methods.py 7:39:32
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 54_class_methods.py
+++ 54_class_methods.py
@@ -7,9 +7,28 @@
 class Student:
 
     count = 0  # Class variable to keep track of the number of students
+    total_gpa = 0  # Class variable to keep track of the total GPA
 
     def __init__(self, name, gpa):
         self.name = name
         self.gpa = gpa
         Student.count += 1 # Increment the count of students whenever a new student is created
+        Student.total_gpa += gpa
+
+        #INSTANCE METHOD
+    def get_info(self):
+        return f"Student: {self.name}, GPA: {self.gpa}"
+
+    @classmethod
+    def get_count(cls):
+        return f"Total number of students: {cls.count}"
+
+
+student1 = Student("Spongebob", 3.2)
+student2 = Student("Patrick", 2.0)
+student3 = Student("Sandy", 4.0)
+
+
+
+print(Student.get_count())
 
