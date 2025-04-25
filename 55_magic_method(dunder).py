b12fa49b323168ab5733ac06322fa12debe28772
franců <francupatrik@seznam.cz>
2025-04-25 11:32:11 +0200

    55_magic_method.py 7:46:17
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 55_magic_method(dunder).py
@@ -0,0 +1,28 @@
+#Magic methods = dunder methods (__ double underscore), __init__, __str__, __eq__
+#               They are automatically canlled by many Pythons built in operations.
+#               They allow developers to define or customize the behavior of objects.
+
+
+
+
+class Book:
+
+    def __init__(self, title, author, num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
+
+    def __str__(self):
+        return f"{self.title} by {self.author}." #string representation of the object, we can print it(otherwise it would print the
+
+
+    def __eq__(self, other):#equals, self and other book
+        return self.title == other.title and self.author == other.author
+
+book1 = Book("The Hobbit", "J.R.R. Tolkien", 310) #instance třídy BOOK
+book2 = Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223) #instance třídy BOOK
+book3 = Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 172) #instance třídy BOOK
+book4 = Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 172)
+
+print(book1)
+print(book3 == book4)
--- 55_magic_method.py
+++
@@ -1,3 +0,0 @@
-#Magic methods = dunder methods (__ double underscore), __init__, __str__, __eq__
-#               They are automatically canlled by many Pythons built in operations.
-#               They allow developers to define or customize the behavior of objects.
