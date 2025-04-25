d511efa791efbde194a1e69418409e736ffdf95d
franců <francupatrik@seznam.cz>
2025-04-25 11:48:56 +0200

    56_property.py 7:59:52
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 56_property.py
@@ -0,0 +1,3 @@
+# @property = Decorator used to define a method as a property (it can be accessed like and attribute)
+# #           Benefit: Add additional logic when read, write or delete attributes
+# #           Gives you getter, setter and deleter method
