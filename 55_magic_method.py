c3f101b29258aa1cbaaf4b00bc535567ddeb19e8
franců <francupatrik@seznam.cz>
2025-04-25 11:23:40 +0200

    55_magic_method.py 7:46:17
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 55_magic_method.py
@@ -0,0 +1,3 @@
+#Magic methods = dunder methods (__ double underscore), __init__, __str__, __eq__
+#               They are automatically canlled by many Pythons built in operations.
+#               They allow developers to define or customize the behavior of objects.
