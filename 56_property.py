94709e66309d9463dc831cb0437459fc0aa136de
franců <francupatrik@seznam.cz>
2025-04-25 12:08:03 +0200

    56_property.py 7:59:52
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 56_property.py
+++ 56_property.py
@@ -1,3 +1,26 @@
 # @property = Decorator used to define a method as a property (it can be accessed like and attribute)
 # #           Benefit: Add additional logic when read, write or delete attributes
 # #           Gives you getter, setter and deleter method
+
+
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height #_.height = protected attribute, its private
+        #jsou private, neměli by se použít 'natrvrdo' ale uvnitř methody
+
+    @property
+    def width(self):
+        return f" {self._width:.1f}cm"
+
+    @property
+    def height(self):
+        return f" {self._height:.1f}cm"
+
+
+
+rectangle = Rectangle(3, 4)
+print(rectangle.width)
+print(rectangle.height)
