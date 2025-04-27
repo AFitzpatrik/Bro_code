233e693722294313e7fca1a7287bfbbb0d46a573
franců <francupatrik@seznam.cz>
2025-04-27 15:18:24 +0200

    58_exception_handling.py 8:15:01
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 58_exception_handling.py
+++ 58_exception_handling.py
@@ -1,3 +1,13 @@
 # exception = Aa event that interrupts the flow of a program
 #             (ZeroDivisionError, ValueError, TypeError, FileNotFoundError, IndexError...)
 #             1. try,  2. except,  3.finally
+
+
+try:
+    number = int(input("Enter a number: "))
+    print(1 / number)
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+except ValueError:
+    print("Please enter a valid number!")
+
