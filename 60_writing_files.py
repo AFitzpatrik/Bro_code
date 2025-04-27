0d05faca2ab63d9ffcb1f073bcf4d905481ae9a1
franců <francupatrik@seznam.cz>
2025-04-27 21:25:59 +0200

    60_writing_files.py 8:27:48
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 60_writing_files.py
+++ 60_writing_files.py
@@ -1,1 +1,14 @@
 #Python writing files (.txt, .json, .csv)
+
+
+txt_data = "I like pizza"
+file_path = "output.txt"
+
+# VYTVOŘENÍ SOUBORU A NAPSÁNÍ TEXTU DO NĚJ (Text = i like pizza, soubor = output.txt)
+with open(file=file_path,mode= "w") as file: # w = write, a = append, r = read, x = exclusive creation
+    file.write(txt_data)
+    print(f"{txt_data} was written to {file_path}")
+
+
+
+
