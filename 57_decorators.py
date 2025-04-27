fd5057f6254ed195623d4f4662cdd71ef8413339
franců <francupatrik@seznam.cz>
2025-04-27 15:06:05 +0200

    57_decorators.py 8:08:01
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 57_decorators.py
+++ 57_decorators.py
@@ -4,3 +4,23 @@
 
 #            @add_sprinkles
 #            get_ice_cream("vanilla")
+
+def add_sprinkles(func):
+    def wrapper():
+        print("Adding sprinkles!")
+        func()
+    return wrapper
+
+def add_fudge(func):
+    def wrapper():
+        print("Adding fudge!")
+        func()
+    return wrapper
+
+
+@add_sprinkles #extends the behavior of get_ice_cream
+def get_ice_cream():
+    print("Here is your ice cream!")
+
+
+get_ice_cream()
