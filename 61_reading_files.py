ad50e0a0787ba4be2b4f9e012247a211a34c819b
franců <francupatrik@seznam.cz>
2025-04-27 21:59:13 +0200

    61_reading_files.py 8:41:38
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 61_reading_files.py
+++ 61_reading_files.py
@@ -1,8 +1,45 @@
 # Pythong reading files (.txt, .json, .csv)
+import json
+#file_path = "employees.txt"
 
-file_path = "employees.txt"
+'''
+try:
+    with open(file_path, mode="r") as file:#r= read
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print(f"{file_path} was not found")
+except PermissionError:
+    print(f"{file_path} has limited access to read it")
+'''
+#---------------------------------------------------------------------------------------------------------
+#JSON
+'''
+file_path = "employees.json"
+try:
+    with open(file_path, mode="r") as file:#r= read
+        content = json.load(file)
+        print(content["name"])
+        #print(content) # přečte vše
+except FileNotFoundError:
+    print(f"{file_path} was not found")
+except PermissionError:
+    print(f"{file_path} has limited access to read it")
+    '''
+#---------------------------------------------------------------------------------------------------------
+#CSV
+import csv
+file_path = "employees.csv"
+
+try:
+    with open(file_path, mode="r") as file:#r= read
+        content = csv.reader(file)
+        for line in content:
+            #print(line[0])
+            print(line)
+except FileNotFoundError:
+    print(f"{file_path} was not found")
+except PermissionError:
+    print(f"{file_path} has limited access to read it")
 
 
-with open(file_path, mode="r") as file:#r= read
-    content = file.read()
-    print(content)
