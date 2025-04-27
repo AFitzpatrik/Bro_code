53770b9600e206380aabbe7f81c64d88218705ab
franců <francupatrik@seznam.cz>
2025-04-27 15:47:37 +0200

    59_file_detection.py 8:20:50
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 59_file_detection.py
@@ -0,0 +1,16 @@
+#python file decection
+import os
+
+file_path = "stuff/test_detection.txt"  #relative path, když je soubor ve stejném adresáři jako skript
+file_path2 = "C:/Users/franc/Desktop/bro.txt" #absolute path, když je soubor v jiném adresáři než skript
+
+if os.path.exists(file_path2):
+    print (f"The location '{file_path2}' exists")
+
+    if os.path.isfile(file_path2):
+        print("That is a file")
+
+    elif os.path.isdir(file_path2):
+        print("That is a directory")
+else:
+    print (f"The location '{file_path2}' does not exist")
