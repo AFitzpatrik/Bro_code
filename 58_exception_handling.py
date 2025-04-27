d2b4ab0c988f2ed4ce8f7b4cebc8092efeaa3c71
franců <francupatrik@seznam.cz>
2025-04-27 15:14:02 +0200

    58_exception_handling.py 8:15:01
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 58_exception_handling.py
+++ 58_exception_handling.py
@@ -1,3 +1,3 @@
 # exception = Aa event that interrupts the flow of a program
-#             (ZeroDivisionError, ValueError, TypeError, FileNotFoundError, IndexError)
+#             (ZeroDivisionError, ValueError, TypeError, FileNotFoundError, IndexError...)
 #             1. try,  2. except,  3.finally
