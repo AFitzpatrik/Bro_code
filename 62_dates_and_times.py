90956c94ec3c649b240893661caaf1ed3c87830e
franců <francupatrik@seznam.cz>
2025-04-28 22:07:07 +0200

    63_alarm_clock.py 8:54:47
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 62_dates_and_times.py
+++ 62_dates_and_times.py
@@ -1,1 +1,29 @@
-#
+import datetime
+
+date = datetime.date(2025, 1, 2)
+today = datetime.date.today()
+
+time = datetime.time(12, 30, 59)
+now = datetime.datetime.now()
+
+now = now.strftime("%H:%M:%S %d-%m-%y") # format time
+
+
+target_datetime = datetime.datetime(2025, 4, 28, 12, 30, 1)
+current_daytime = datetime.datetime.now()
+
+if target_datetime > current_daytime:
+    print("You need to wait!")
+elif target_datetime < current_daytime:
+    print("You missed the date!")
+else:
+    print("its NOW!!")
+
+
+
+
+
+print(date)
+print(today)
+print(time)
+print(now)
