a2f1e2f07ab093c3df693d129be723ed613d9709
franců <francupatrik@seznam.cz>
2025-04-29 21:33:38 +0200

    63_alarm_clock 8:56:45
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 63_alarm_clock/main.py
+++ 63_alarm_clock/main.py
@@ -4,7 +4,7 @@ import datetime
 import pygame
 
 def set_alarm(alarm_time): #Funkce přijímá jeden argument, alarm_time což je čas, který uživatel zadá v inputu
-    pass
+    print(f"Alarm set for {alarm_time}!")
 
 
 
@@ -18,3 +18,4 @@ def set_alarm(alarm_time): #Funkce přijímá jeden argument, alarm_time což je
 
 if __name__ == "__main__":
     alarm_time = input("Enter the alarm time in 24 hour format (HH:MM:SS): ")
+    set_alarm(alarm_time)
