edd02b63e45e1db7ca7b7a86e9d8aca79575f101
franců <francupatrik@seznam.cz>
2025-04-29 21:35:55 +0200

    63_alarm_clock 8:56:45
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 63_alarm_clock/main.py
+++ 63_alarm_clock/main.py
@@ -19,3 +19,5 @@ def set_alarm(alarm_time): #Funkce přijímá jeden argument, alarm_time což je
 if __name__ == "__main__":
     alarm_time = input("Enter the alarm time in 24 hour format (HH:MM:SS): ")
     set_alarm(alarm_time)
+
+
