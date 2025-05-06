a63c54061771a0b6491f65c7c1d943417c14d9bd
franců <francupatrik@seznam.cz>
2025-05-06 11:20:32 +0200

    63_alarm_clock 9:05:04
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

---
+++ 64_multihreading.py
@@ -0,0 +1,3 @@
+#Multithreading = Used to perform multiple tasks concurrently (multitasking)
+#                 Good for I/O bound tasks like reading file or festching data from API
+#                 threading.Thread(target=m_function)
