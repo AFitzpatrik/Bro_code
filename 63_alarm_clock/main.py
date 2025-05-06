7d47cd23bcbafcd34b4876d1d45bc0ead7bf67e8
franců <francupatrik@seznam.cz>
2025-05-06 10:57:51 +0200

    63_alarm_clock 8:56:45
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 63_alarm_clock/main.py
+++ 63_alarm_clock/main.py
@@ -6,6 +6,13 @@ import pygame
 def set_alarm(alarm_time): #Funkce přijímá jeden argument, alarm_time což je čas, který uživatel zadá v inputu
     print(f"Alarm set for {alarm_time}!")
     sound_file = "my_music.mp3"  # Cesta na soubor ve složce
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")  # Získání aktuálního času ve formátu HH:MM:SS
+        print(current_time)
+
+        time.sleep(1)
 
 
 
