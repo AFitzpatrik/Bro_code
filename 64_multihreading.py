3e8acc435bcc5353b1840d8251f180f6b432eccb
franců <francupatrik@seznam.cz>
2025-05-06 11:30:14 +0200

    63_alarm_clock 9:05:04
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 64_multihreading.py
+++ 64_multihreading.py
@@ -1,3 +1,39 @@
 #Multithreading = Used to perform multiple tasks concurrently (multitasking)
-#                 Good for I/O bound tasks like reading file or festching data from API
+#                 Good for I/O(input,output) bound tasks like reading file or festching data from API
 #                 threading.Thread(target=m_function)
+
+
+import threading
+import time
+
+def walk_dog():
+    time.sleep(8)
+    print("You finish walking the dog")
+
+def take_out_trash():
+    time.sleep(2)
+    print("You finish taking out the trash")
+
+def get_mail():
+    time.sleep(4)
+    print("You finish getting the mail")
+
+chore1 = threading.Thread(target=walk_dog)
+chore1.start()
+
+chore2 = threading.Thread(target=take_out_trash)
+chore2.start()
+
+chore3 = threading.Thread(target=get_mail)
+chore3.start()
+
+
+
+'''
+walk_dog()
+take_out_trash()
+get_mail()
+- Když to udělám takto, na jednom threadu, první se dokončí walking_dog, pak taking out trash a nakonec getting the mail
+pokud bych to udělal přes multithreading, tak se první dokončí taking out trash, pak getting the mail a nakonec walking dog
+'''
+
