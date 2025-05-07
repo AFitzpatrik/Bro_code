14d59c71e069f9cc4cd9e9ac12183f5249ca57c1
franců <francupatrik@seznam.cz>
2025-05-07 22:14:13 +0200

    65_request_API_data.py 9:16:06
    https://www.youtube.com/watch?v=ix9cRaBkVe0&t=27579s
    
    https://www.youtube.com/watch?v=97V7ICVeTJc
    
    Course: https://www.youtube.com/watch?v=ix9cRaBkVe0&t=745s
    
    Exercises : https://www.youtube.com/watch?v=4wGuB3oAKc4

--- 65_request_API_data.py
+++ 65_request_API_data.py
@@ -1,13 +1,27 @@
-#https://pokeapi.co/
+# https://pokeapi.co/
 
 import requests
 
 base_url = "https://pokeapi.co/api/v2/"
 
-def get_pokemon_info(name): #name je parametr funkce
-    url = f"{base_url}/pokemon/{name}" #vytvoříme url adresu
-    response = requests.get(url)
-    print(response)
 
-pokemon_name = "pikachu" #pokemon_name je argument
-get_pokemon_info(pokemon_name)
+def get_pokemon_info(name):  # name je parametr funkce
+    url = f"{base_url}/pokemon/{name}"  # vytvoříme url adresu
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
+
+pokemon_name = "pikachu"  # pokemon_name je argument
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info['name']}")
+    print(f"ID: {pokemon_info['id']}")
+    print(f"Height: {pokemon_info['height']}")
+    print(f"Weight: {pokemon_info['weight']}")
+    print(f"Abilities: {[ability['ability']['name'] for ability in pokemon_info['abilities']]}")
